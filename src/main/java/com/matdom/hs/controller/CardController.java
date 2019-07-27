47fbf711ffb200942c537ea7937853c2ec175101
dominik bokowy <dominik.bokowy@gmail.com>
2019-07-27 13:33:35 +0200

    cors added for debug

--- src/main/java/com/matdom/hs/controller/CardController.java
+++ src/main/java/com/matdom/hs/controller/CardController.java
@@ -4,16 +4,11 @@ import com.matdom.hs.dtos.CardDTO;
 import com.matdom.hs.mapper.CardMapper;
 import com.matdom.hs.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/cards")
 public class CardController {
--- src/main/java/com/matdom/hs/controller/DeckController.java
+++ src/main/java/com/matdom/hs/controller/DeckController.java
@@ -4,16 +4,11 @@ import com.matdom.hs.dtos.DeckDTO;
 import com.matdom.hs.mapper.DeckMapper;
 import com.matdom.hs.service.DeckService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/decks")
 public class DeckController {
